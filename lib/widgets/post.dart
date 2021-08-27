5abcc3fb2806033bb665295d6cc0bfb42d73f94f
Tanvir Mahin <55342128+TanvirMahin24@users.noreply.github.com>
2021-08-27 19:49:36 +0600

    post model add

--- lib/widgets/post.dart
+++ lib/widgets/post.dart
@@ -1,11 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Post extends StatefulWidget {
+  late final String postId;
+  late final String ownerId;
+  late final String username;
+  late final String location;
+  late final String description;
+  late final String mediaUrl;
+  late final dynamic likes;
+
+  Post({
+    required this.postId,
+    required this.ownerId,
+    required this.username,
+    required this.location,
+    required this.description,
+    required this.likes,
+    required this.mediaUrl,
+  });
+
+  factory Post.fromDocument(DocumentSnapshot doc) {
+    return Post(
+      postId: doc['postId'],
+      ownerId: doc['ownerId'],
+      username: doc['username'],
+      location: doc['location'],
+      description: doc['description'],
+      likes: doc['likes'],
+      mediaUrl: doc['mediaUrl'],
+    );
+  }
+
+  int getLikeCount(likes) {
+    if (likes == null) {
+      return 0;
+    }
+    int count = 0;
+    likes.values.forEach((val) {
+      if (val == true) {
+        count++;
+      }
+    });
+    return count;
+  }
+
   @override
-  _PostState createState() => _PostState();
+  _PostState createState() => _PostState(
+      postId: this.postId,
+      ownerId: this.ownerId,
+      username: this.username,
+      location: this.location,
+      description: this.description,
+      mediaUrl: this.mediaUrl,
+      likes: this.likes,
+      likeCount: getLikeCount(this.likes));
 }
 
 class _PostState extends State<Post> {
+  late final String postId;
+  late final String ownerId;
+  late final String username;
+  late final String location;
+  late final String description;
+  late final String mediaUrl;
+
+  late Map likes;
+  late int likeCount;
+
+  _PostState(
+      {required this.postId,
+      required this.ownerId,
+      required this.username,
+      required this.location,
+      required this.description,
+      required this.mediaUrl,
+      required this.likes,
+      required this.likeCount});
+
   @override
   Widget build(BuildContext context) {
     return Text("Post");
